0173fe132373320a87eaa3f2c658d14b93697f3f
Eliezerelc <candidoeliezer0@gmail.com>
2022-05-25 21:56:00 -0300

    Initial commit

---
+++ codicao.js
@@ -0,0 +1,9 @@
+var idade = 25
+console.log('vc tem ',idade)
+if(idade < 14){
+    console.log('vc nao pode votar')
+}else if(idade < 18 || idade > 65){
+        console.log('seu voto e opcinal')
+    }else{
+        console.log('voto e obrigatorio') 
+    }
---
+++ e001.js
@@ -0,0 +1,6 @@
+var e = 'eua'
+if( e == 'brasil'){
+ console.log('vc e brasileiro')
+}else {
+    console.log('vc e estrangeiro')
+}
